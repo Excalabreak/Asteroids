dc294ea56311366b030007d0c8f2e05b23240268
Justin Lam <jlam123321@gmail.com>
2024-02-07 19:11:48 -0800

    comment code

--- Asteroids_Lam_Justin/Assets/Scripts/InputManager.cs
+++ Asteroids_Lam_Justin/Assets/Scripts/InputManager.cs
@@ -2,12 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+ * Author: [Lam, Justin]
+ * Last Updated: [02/07/2024]
+ * [gets all the inputs from the input system and calls functions to handle them]
+ */
 public class InputManager : MonoBehaviour
 {
     PlayerControls _playerControls;
 
     public Vector2 _moveInput;
 
+    /// <summary>
+    /// on enable:
+    /// makes new PlayerController if there isnt already one
+    /// sets up callback contexts
+    /// 
+    /// enables _playerController
+    /// </summary>
     private void OnEnable()
     {
         if (_playerControls == null)
@@ -20,6 +32,9 @@ public class InputManager : MonoBehaviour
         _playerControls.Enable();
     }
 
+    /// <summary>
+    /// disables _playerController when gameobject is disabled
+    /// </summary>
     private void OnDisable()
     {
         _playerControls.Disable();
