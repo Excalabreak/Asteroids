34089c9518828f646d731e9ffcc7e381f040019b
Justin Lam <jlam123321@gmail.com>
2024-02-13 18:54:56 -0800

    added screen wrap

---
+++ Asteroids_Lam_Justin/Assets/Scripts/ScreenWrap.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Author: [Lam, Justin]
+ * Last Updated: [2/13/2024]
+ * [Any gameobject with this script, will wrap around the screen]
+ */
+
+[RequireComponent(typeof(Rigidbody))]
+public class ScreenWrap : MonoBehaviour
+{
+    private Rigidbody objectRigidbody;
+
+    private void Awake()
+    {
+        objectRigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        Vector3 screenPos = Camera.main.WorldToScreenPoint(transform.position);
+
+        Vector3 topRight = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0f));
+        Vector3 bottomLeft = Camera.main.ScreenToWorldPoint(new Vector3(0f, 0f, 0f));
+
+        if (screenPos.x <= 0 && objectRigidbody.velocity.x < 0)
+        {
+            transform.position = new Vector3(topRight.x, transform.position.y, 0f);
+        }
+        else if (screenPos.x >= Screen.width && objectRigidbody.velocity.x > 0)
+        {
+            transform.position = new Vector3(bottomLeft.x, transform.position.y, 0f);
+        }
+        if (screenPos.y >= Screen.height && objectRigidbody.velocity.y > 0)
+        {
+            transform.position = new Vector3(transform.position.x, bottomLeft.y, 0f);
+        }
+        else if (screenPos.y <= 0 && objectRigidbody.velocity.y < 0)
+        {
+            transform.position = new Vector3(transform.position.x, topRight.y, 0f);
+        }
+    }
+}
