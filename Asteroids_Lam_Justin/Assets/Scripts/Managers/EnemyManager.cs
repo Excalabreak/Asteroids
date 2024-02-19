b3f6f352ccf89be2e4a7faf585a408a8f46931e5
Justin Lam <jlam123321@gmail.com>
2024-02-18 19:49:29 -0800

    game manager deletes it self?

--- Asteroids_Lam_Justin/Assets/Scripts/Managers/EnemyManager.cs
+++ Asteroids_Lam_Justin/Assets/Scripts/Managers/EnemyManager.cs
@@ -10,6 +10,7 @@ using UnityEngine;
 
 public class EnemyManager : Singleton<EnemyManager>
 {
+    //enemy prefabs
     [SerializeField] private GameObject _bigAsteroidPrefab;
     [SerializeField] private GameObject _bigUFOPrefab;
     [SerializeField] private GameObject _smallUFOPrefab;
@@ -17,6 +18,11 @@ public class EnemyManager : Singleton<EnemyManager>
     //all enemies
     private List<GameObject> _currentAsteroids;
     private GameObject _currentUFO;
+
+    //ufo random spawn variables
+    [SerializeField] private float _maxUFOSpawnDelay = 10f;
+    private bool startSpawningUFO = false;
+    private bool spawningUFO = false;
 
     /// <summary>
     /// sets up list
@@ -33,7 +39,7 @@ public class EnemyManager : Singleton<EnemyManager>
     /// </summary>
     public void SpawnEnemies()
     {
-        int asteroidAmount = 3 + PlayerData.Instance.currentLevel - 1;
+        int asteroidAmount = 1 + PlayerData.Instance.currentLevel - 1;
 
         for (int index = 0; index < asteroidAmount; index++)
         {
@@ -45,10 +51,10 @@ public class EnemyManager : Singleton<EnemyManager>
 
             _currentAsteroids.Add(asteroid);
         }
-        //instantiate asteroids
-        //start UFO cycle
-    }
 
+        StartCoroutine(SpawnUFO(_maxUFOSpawnDelay));
+        startSpawningUFO = true;
+    }
     /*
      * note:
      * when spawning ufo:
@@ -58,4 +64,38 @@ public class EnemyManager : Singleton<EnemyManager>
      * repeat
      * spawn from one side of the screen
      */
+
+    /// <summary>
+    /// Instantiates UFO Prefab after a delay
+    /// </summary>
+    /// <param name="seconds">how long to wait to spawn next UFO</param>
+    /// <returns></returns>
+    private IEnumerator SpawnUFO(float seconds)
+    {
+        spawningUFO = true;
+        yield return new WaitForSeconds(seconds);
+
+        float randomNum = Random.Range(0, 1);
+        Vector3 topRight = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0f));
+        Vector3 bottomLeft = Camera.main.ScreenToWorldPoint(new Vector3(0f, 0f, 0f));
+
+        if (randomNum < .25f)
+        {
+            _currentUFO = Instantiate(_smallUFOPrefab, new Vector3(0f, topRight.y + 5, 0f), Quaternion.identity);
+        }
+        else
+        {
+            _currentUFO = Instantiate(_smallUFOPrefab, new Vector3(0f, topRight.y + 5, 0f), Quaternion.identity);
+        }
+
+        BaseUFOScript currentUFOScript = _currentUFO.GetComponent<BaseUFOScript>(); 
+        if (currentUFOScript.goingRight)
+        {
+            _currentUFO.transform.position = new Vector3(bottomLeft.x - 5, Random.Range(bottomLeft.y, topRight.y), 0f);
+        }
+        else
+        {
+            _currentUFO.transform.position = new Vector3(topRight.x - 5, Random.Range(bottomLeft.y, topRight.y), 0f);
+        }
+    }
 }
